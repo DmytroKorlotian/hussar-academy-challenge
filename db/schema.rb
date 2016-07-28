117fa686b9df1f73dba7c05334fbb0bf8b9c5ff5
Dmytro Korlotian <dima@korlotian.pl>
2016-07-28 11:38:17 +0200

    add like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610104952) do
+ActiveRecord::Schema.define(version: 20160728093649) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140610104952) do
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
   add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+
+  create_table "likes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "title"
